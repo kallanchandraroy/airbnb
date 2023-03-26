a30f292c944c5003a38d7a41e8c4a8f15e09c116
Kallan Chandra Roy <shuvoroy123ok@gmail.com>
2023-03-27 00:57:40 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import IndexPage from "./pages/IndexPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<IndexPage/>} />
+    </Routes>
+    
+  );
+}
+
+export default App;
